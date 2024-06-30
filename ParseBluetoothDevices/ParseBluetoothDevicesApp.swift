feb34115518d79ab365db31aa0460bc7ed6c25b0
James Bennett <james.bennett@decathlon.com>
2024-06-30 11:04:46 +0200

    Initial Commit

---
+++ ParseBluetoothDevices/ParseBluetoothDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParseBluetoothDevicesApp.swift
+//  ParseBluetoothDevices
+//
+//  Created by James BENNETT on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ParseBluetoothDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
